7ad36ee1ff0a15253b5874e877ed41f33d3398d3
ivonet <ivonet@gmail.com>
2014-11-14 16:28:24 +0100

    * updated Copyright * added some pom stuff * made the jar executable

--- src/main/java/com/github/junrar/unpack/Unpack20.java
+++ src/main/java/com/github/junrar/unpack/Unpack20.java
@@ -3,7 +3,7 @@
  * Copyright (c) 2007 innoSysTec (R) GmbH, Germany. All rights reserved.
  * Original author: Edmund Wagner
  *
- * Copyright (c) 2014 IvoNet.nl. All rights reserved
+ * Copyright (c) 2014 IvoNet.nl. All rights reserved.
  * Refactoring and upgrading of original code: Ivo Woltring
  * Author of all nl.ivonet packaged code: Ivo Woltring
  *
